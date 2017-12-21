024e962c8485af79f656140d050d77704597d308
yuqing <592932445@qq.com>
2017-12-21 19:52:15 +0800

    First commit

---
+++ app/src/main/java/com/example/administrator/coolweather/Province.java
@@ -0,0 +1,35 @@
+package com.example.administrator.coolweather;
+
+/**
+ * Created by Administrator on 2017/12/21.
+ */
+
+public class Province {
+    private int id;
+    private String privinceName;
+    private int prvinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPrivinceName() {
+        return privinceName;
+    }
+
+    public void setPrivinceName(String privinceName) {
+        this.privinceName = privinceName;
+    }
+
+    public int getPrvinceCode() {
+        return prvinceCode;
+    }
+
+    public void setPrvinceCode(int prvinceCode) {
+        this.prvinceCode = prvinceCode;
+    }
+}
